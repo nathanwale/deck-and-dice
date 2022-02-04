6b1a065e92eae3b8a99731cf6c9d4ac0a676c88a
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-02-04 17:57:11 +1100

    Group tests

--- src/__tests__/group_test.ts
+++ src/__tests__/group_test.ts
@@ -1,22 +1,24 @@
 import 'jest';
-import * as Deck from '../models/Deck';
-import * as Card from '../models/Card';
+import * as Oracle from '../models/Oracle';
 import * as Group from '../models/Group';
 import * as Geo from '../models/Geometry';
 
-
+function quickopts (text: string) {
+    return Oracle.options_from_names(text.split(""))
+}
 
 test('Group basics', () => {
-    let deck: Deck.Deck = {
-        cards: Card.from_names("ABCD".split(""))
-    };
-    let shuffled_deck: Deck.ShuffledDeck = {
+    let shuffled_deck: Oracle.Oracle = {
         index: 0,
-        cards: Card.from_names("WXYZ".split(""))
+        options: quickopts("ABCD"),
+        style: Oracle.Style.Cards
     }
+
+    let table = Oracle.table_from_options(quickopts("WXYZ"))
+
     let group: Group.Group = {
         title: "Test Group",
-        items: [deck, shuffled_deck],
+        oracles: [table, shuffled_deck],
         frame: { 
             origin: {x: 2, y: 3},
             end: {x: 12, y: 13},
@@ -24,28 +26,38 @@ test('Group basics', () => {
     };
 
     expect(group.title).toEqual("Test Group");
-    expect(group.items).toEqual([deck, shuffled_deck]);
+    expect(group.oracles).toEqual([table, shuffled_deck]);
     expect(group.frame.origin).toEqual({x: 2, y: 3});
     expect(group.frame.end).toEqual({x: 12, y: 13});
 });
 
 test('run all', () => {
-    let deck: Deck.Deck = {
-        cards: Card.from_names("ABCD".split(""))
-    };
-    let shuffled_deck: Deck.ShuffledDeck = {
+    let shuffled_deck: Oracle.Oracle = {
         index: 0,
-        cards: Card.from_names("WXYZ".split(""))
+        options: quickopts("ABCD"),
+        style: Oracle.Style.Cards
     }
+
+    let table = Oracle.table_from_options(quickopts("WXYZ"))
+    let die = Oracle.die_from_range(1, 6);
+
     let group: Group.Group = {
         title: "Test Group",
-        items: [deck, shuffled_deck],
+        oracles: [table, shuffled_deck, die],
         frame: { 
             origin: {x: 2, y: 3},
             end: {x: 12, y: 13},
          }
     };
 
-    // expect(Group.runall(group)).toEqual([])
+    let results: Oracle.Result[] = Group.pickall(group)
+    expect(results.length).toEqual(3)
+    let [[table_result, _table2], [card, _deck], [die_roll, _die]] = results;
+    expect("WXYZ".includes(table_result.name)).toEqual(true);
+    expect("ABCD".includes(card.name)).toEqual(true);
+    expect([1, 2, 3, 4, 5, 6].includes(die_roll.value)).toEqual(true);
+    expect(Group.sum(results)).toBeGreaterThanOrEqual(1);
+    expect(Group.sum(results)).toBeLessThanOrEqual(6);
+    expect([1, 2, 3, 4, 5, 6].includes(Group.sum(results))).toEqual(true);
 });
 
