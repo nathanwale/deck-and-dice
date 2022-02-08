d8f1445f6279cb306d3daed0bb13fc22b8474b9e
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-02-08 20:05:33 +1100

    View added

---
+++ src/view/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './App.css';
+import * as Oracle from '../models/Oracle'
+import * as OracleGroup from '../models/Group'
+import { Group } from './Group';
+
+let oracles = [
+  Oracle.shuffled_deck("Pick a letter", Oracle.options_from_names("A23456789JQK".split(""))),
+  Oracle.die_with_sides("Challenge", 8),
+  Oracle.die_with_sides("Action", 6),
+  Oracle.die_with_sides("Challenge", 8),
+]
+
+let group: OracleGroup.Group = {
+  title: "Group",
+  oracles: oracles,
+  frame: { origin: {x: 0, y: 0}, end: {x: 10, y: 10}}
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Group group={group} />
+    </div>
+  );
+}
+
+export default App;
