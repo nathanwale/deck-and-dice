083a9fdd2ab195b50c4bbd92c791cf326a73a1df
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-02-27 16:25:44 +1100

    Added GroupEditor

---
+++ src/view/GroupEditor.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Group} from './Group'
+import * as GroupModel from '../models/Group';
+
+type Props = {
+    group: GroupModel.Group,
+    on_done: (group: GroupModel.Group) => void
+}
+
+export function GroupEditor(props: Props)
+{
+    return <div className='group-edit'>
+        <Group group={props.group} />
+        <div className='menu'>
+            <input type='text' />
+            <label>
+                <input type='checkbox' checked={false} readOnly={true} />
+                Show total
+            </label>
+        </div>
+        <button className='done' onClick={() => props.on_done(props.group)}>Done</button>
+    </div>
+}
--- src/view/Library.tsx
+++ src/view/Library.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import * as Oracle from '../models/Oracle';
-import * as OracleMap from '../models/OracleMap';
-import * as ViewModel from './ViewModel';
-import {Group} from './Group'
+import {GroupEditor} from './GroupEditor'
 import * as GroupModel from '../models/Group';
 
 type Props = {
@@ -18,12 +16,12 @@ const init_group: GroupModel.Group = {
 
 export function Library(props: Props): JSX.Element
 {
-
     const [group, set_group] = React.useState(init_group);
-
+    
     function add_oracle(oracle: Oracle.Oracle)
     {
-        set_group(GroupModel.add_oracle(group, oracle))
+        let updated_group = GroupModel.add_oracle(group, oracle)
+        set_group(updated_group);
     }
 
     const oracle_buttons = props.oracles.map((oracle, key) => {
@@ -39,15 +37,7 @@ export function Library(props: Props): JSX.Element
             <div className='oracles'>
                 { oracle_buttons }
             </div>
-            <Group group={group} />
-            <div className='menu'>
-                <input type='text' />
-                <label>
-                    <input type='checkbox' checked={false} readOnly={true} />
-                    Show total
-                </label>
-            </div>
-            <button className='done' onClick={() => props.on_done(group)}>Done</button>
+            <GroupEditor group={ group } on_done={ props.on_done } />
         </div>
     </>
 }
